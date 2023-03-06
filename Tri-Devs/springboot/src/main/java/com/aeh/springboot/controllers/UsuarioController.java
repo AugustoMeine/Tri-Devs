b6c18524df6ae813e25fcf5920c3b3859da63a11
Augusto Meine <augusto.meine@gmail.com>
2023-03-06 20:21:30 -0300

    Criação de algumas APIs relacionadas a tela de login (inacabada) e modificação na tela de login (inacaba) - 06/03/2023.

--- Tri-Devs/springboot/src/main/java/com/aeh/springboot/controllers/UsuarioController.java
+++ Tri-Devs/springboot/src/main/java/com/aeh/springboot/controllers/UsuarioController.java
@@ -1,23 +1,41 @@
 package com.aeh.springboot.controllers;
 
 import com.aeh.springboot.models.Usuario;
-import com.aeh.springboot.repositories.UsuarioRepository;
 import com.aeh.springboot.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/Usuario")
 public class UsuarioController {
 
     @Autowired
     UsuarioService usuarioService;
 
-    @GetMapping("")
-    public String paginaInicial(){
-        return("Conectou");
+    @GetMapping({"/",""})
+    public List<Usuario> lerUsuraios(){
+        return(usuarioService.lerUsuarios());
     }
 
+    @GetMapping("/{idUsuario}")
+    public Usuario lerUsuraio(@PathVariable long idUsuario){
+        return(usuarioService.lerUsuario(idUsuario));
+    }
+
+    @PostMapping("/")
+    public Usuario salvarUsuario(@RequestBody Usuario usuario){
+        return(usuarioService.salvarUsuario(usuario));
+    }
+
+    @DeleteMapping("/")
+    public String deletarUsuario(@RequestBody Usuario usuario){
+        if(usuarioService.deletarUsuario(usuario)){
+            return("Usuario " + usuario.getNome() + " foi deletado com sucesso");
+        }
+        else{
+            return("Usuario " + usuario.getNome() + " não foi deletado");
+        }
+    }
 }
