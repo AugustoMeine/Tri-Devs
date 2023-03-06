14264057b3b5aa87df0cb1035eed147c34d4054f
Augusto Meine <augusto.meine@gmail.com>
2023-03-06 20:21:30 -0300

    Criação de algumas APIs relacionadas a tela de login (inacabada) e modificação na tela de login (inacaba) - 06/03/2023.

--- Tri-Devs/springboot/src/main/java/com/aeh/springboot/models/Usuario.java
+++ Tri-Devs/springboot/src/main/java/com/aeh/springboot/models/Usuario.java
@@ -3,6 +3,8 @@ package com.aeh.springboot.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "USUARIO")
 @Data
@@ -10,15 +12,21 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idUsuario")
     long idUsuario;
 
-    @Column(nullable = false,length = 50)
-    String login;
+    @Column(nullable = false)
+    private String login;
 
-    @Column(nullable = false,length = 50)
-    String senha;
+    @Column(nullable = false)
+    private String senha;
 
-    @Column(nullable = false,length = 150)
-    String nome;
+    @Column(nullable = false)
+    private String nome;
 
+    @Column(nullable = false)
+    private Date dataCriacao;
+
+    @Column(nullable = false)
+    private Date dataDesligamento;
 }
