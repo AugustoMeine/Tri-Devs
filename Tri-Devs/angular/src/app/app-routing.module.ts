e95444c2b32b8d7a7838aec89c52110825933918
Henrique Bolnaldo <henrique.bolnado@atos.net>
2023-03-26 21:02:20 -0300

    Finalizado a pagina de pedidos, direcionamento para as paginas dos botoes em pedidos e começo da tela de comanda/cardapio - 26/03 - Henrique Bolnaldo

--- Tri-Devs/angular/src/app/app-routing.module.ts
+++ Tri-Devs/angular/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
+import { PedidosComponent } from './pedidos/pedidos.component';
 import { DirecionamentoComponent } from './direcionamento/direcionamento.component';
 import { CozinhaComponent } from './cozinha/cozinha.component';
 import { LoginComponent } from './login/login.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComandaMesaComponent } from './pedidos/comandaMesa/comandaMesa.component';
+import { CardapioComponent } from './pedidos/cardapio/cardapio.component';
 
 const routes: Routes = [
   {path:"",component:LoginComponent},
   {path:"Cozinha",component:CozinhaComponent},
-  {path:"Direcionamento" , component:DirecionamentoComponent}
+  {path:"Direcionamento",component:DirecionamentoComponent},
+  {path:"Pedidos",component:PedidosComponent},
+  {path:"Comanda",component:ComandaMesaComponent},
+  {path:"Cardapio",component:CardapioComponent}
 ];
 
 @NgModule({
